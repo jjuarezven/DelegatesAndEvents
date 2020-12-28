51da84f716a2726da567bc07e199bf6c62b921eb
Jose G. Juarez R <josejuarez@outlook.com>
2020-12-28 14:13:26 -0500

    Last invocation

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -3,7 +3,7 @@
 namespace DelegatesAndEvents
 {
     // 1 Declaration
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
     class Program
     {
         static void Main(string[] args)
@@ -14,7 +14,9 @@ namespace DelegatesAndEvents
             WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
             del1 += del2 + del3;
-            del1(5, WorkType.Golf);
+            int totalHours = del1(5, WorkType.Golf);
+            // totalHours get the value of the last invoked delegate
+            Console.WriteLine(totalHours);
             //DoWork(del2);
             Console.Read();
         }
@@ -25,19 +27,22 @@ namespace DelegatesAndEvents
             del(8, WorkType.GenerateReports);
         }
 
-        static void WorkPerformed1(int hours, WorkType workType)
+        static int WorkPerformed1(int hours, WorkType workType)
         {
             Console.WriteLine($"WorkPerformed1 called: {hours}");
+            return hours + 1;
         }
 
-        static void WorkPerformed2(int hours, WorkType workType)
+        static int WorkPerformed2(int hours, WorkType workType)
         {
             Console.WriteLine($"WorkPerformed2 called: {hours}");
+            return hours + 2;
         }
 
-        static void WorkPerformed3(int hours, WorkType workType)
+        static int WorkPerformed3(int hours, WorkType workType)
         {
             Console.WriteLine($"WorkPerformed3 called: {hours}");
+            return hours + 3;
         }
     }
 
