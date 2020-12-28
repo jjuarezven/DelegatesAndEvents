a65d1d9fa7201f9494012f9f965fc66bc2fe26d4
Jose G. Juarez R <josejuarez@outlook.com>
2020-12-28 13:11:55 -0500

    Add project files.

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    // 1 Declaration
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 Instantiation
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            DoWork(del2);
+            Console.Read();
+        }
+
+        private static void DoWork(WorkPerformedHandler del)
+        {
+            // 3 Invocation
+            del(8, WorkType.GenerateReports);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed1 called: {hours}");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed2 called: {hours}");
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
